1832e1d8874374d219112f07c5513ebd63e96c6d
Adam Billett <adam@mvpsohio.com>
2023-11-29 15:30:38 -0500

    inital commit

---
+++ testdb/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+# Create your models here.
+class Users(models.Model):
+    username = models.CharField(max_length=50)
+    user_pass = models.CharField()
+
+
+class TestingTable(models.Model):
+    testVar = models.CharField(max_length=5)
+    age = models.IntegerField()
+
