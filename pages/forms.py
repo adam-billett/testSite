b5ce9afc66f34fedca7852905ffb653f899c1a58
Adam Billett <adam@mvpsohio.com>
2023-12-18 11:52:33 -0500

    adding a login page and a forms.py in order to handle the credintials

---
+++ pages/forms.py
@@ -0,0 +1,6 @@
+from django import forms
+from django.contrib.auth.forms import AuthenticationForm
+
+class LoginForm(AuthenticationForm):
+    # You can add additional customization here if needed
+    pass
--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,7 @@
 # pages/views.py
-
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+from .forms import LoginForm
 
 
 # Create views here
@@ -25,7 +26,22 @@ def careers(request):
 
 
 def login(request):
-    return render(request, 'pages/login.html', {})
+    if request.method == 'POST':
+        form = LoginForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
+
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+            else:
+                form.add_error(None, 'Invalid username or password')
+
+    else:
+        form = LoginForm()
+    return render(request, 'pages/login.html', {'form': form})
 
 
 def blog(request):
